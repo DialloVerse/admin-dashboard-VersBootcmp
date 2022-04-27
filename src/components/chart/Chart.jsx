d0afb1ff56c3edc1c909e20f70eb6547fd957735
DialloVerse <durali@ymail.com>
2022-04-27 02:18:25 +0200

    UpdatedPageInterface Commit

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,10 @@
+import "./chart.css"
+export default function Chart() {
+
+    
+  return (
+    <div className="chart">
+        <h3 className="chartTitle">Sales Analytics</h3>
+    </div>
+  )
+}
