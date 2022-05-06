b1ff5392511a2b0c0c8dc657d46e10e008b67719
DialloVerse <durali@ymail.com>
2022-05-06 15:55:42 +0200

    LatestSuccessfulReview

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,57 +1,63 @@
-import "./chart.css"
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts"
+import { Line, LineChart, ResponsiveContainer, XAxis } from "recharts";
+import "./chart.css";
 
 export default function Chart() {
     
     const data = [
         {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
+          name: "Jan",
+          "Active User": 4000,
         },
         {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
+          name: "Feb",
+          "Active User": 5000,
         },
         {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
+          name: "Mar",
+          "Active User": 3000,
         },
         {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
+          name: "Apr",
+          "Active User": 4000,
         },
         {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
+          name: "Jun",
+          "Active User": 2000,
         },
         {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
+          name: "Jul",
+          "Active User": 4000,
         },
         {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
+          name: "Aug",
+          "Active User": 3000,
         },
+        {
+          name: "Sep",
+          "Active User": 4000,
+        },
+        {
+          name: "Oct",
+          "Active User": 1000,
+        },
+        {
+          name: "Nov",
+          "Active User": 4000,
+        },
+        {
+          name: "Dec",
+          "Active User": 3000,
+        },
+       
       ];
   return (
     <div className="chart">
-        <h3 className="chartTitle">Sales Analytics</h3>
+        <h3 className="chartTitle">User Analytics</h3>
         <ResponsiveContainer width="100%" aspect={4 / 1}>
-            <LineChart data={data}></LineChart>
+            <LineChart data={data}>
+              <XAxis dataKey="name" stroke="#5550bd"/>
+              <Line type="monotone" dataKey="Active User" stroke="#5550bd"/>
+            </LineChart>
         </ResponsiveContainer>
     </div>
   )
