75ce96497fa60de89949c3fb4f9de4d9c4d5345a
DialloVerse <durali@ymail.com>
2022-05-07 18:58:36 +0200

    Finally Resolved React-Dom-Issues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./app.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./app.css"
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-      <div>
-    <Topbar />
-    <div className="container">
-      <Sidebar />
-      <Home/>
-    </div>
-    </div>
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
