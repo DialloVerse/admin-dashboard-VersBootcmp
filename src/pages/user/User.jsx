775699ca1da47e612d618359d7e1cc11250c9720
DialloVerse <durali@ymail.com>
2022-05-08 23:06:53 +0200

    UserDossierCreationNeatlyCompiled

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,4 +1,5 @@
-import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid } from '@mui/icons-material';
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid, Publish } from '@mui/icons-material';
+import { Link } from '@mui/material';
 import "./user.css";
 
 export default function User() {
@@ -6,7 +7,9 @@ export default function User() {
     <div className="user">
         <div className="userTitleContainer">
             <h1 className="userTitle">Edit User</h1>
+            <Link to="/newUser"> 
             <button className="userAddButton">Create</button>
+            </Link>
         </div>
         <div className="userContainer">
             <div className="userShow">
@@ -34,7 +37,7 @@ export default function User() {
                     <span className="userShowTitle">Contact Details</span>
                     <div className="userShowInfo">
                     <PhoneAndroid className="userShowIcon"/>
-                    <span className="userShowInfoTitle">+49 1234 274 33</span>
+                    <span className="userShowInfoTitle">+49 1234 567 89</span>
                     </div>
                     <div className="userShowInfo">
                     <MailOutline className="userShowIcon"/>
@@ -46,7 +49,65 @@ export default function User() {
                     </div>
                 </div>
             </div>
-            <div className="userUpdate"></div>
+            <div className="userUpdate">
+                <span className="userUpdateTitle">Edit</span>
+                <form className="userUpdateForm">
+                    <div className="userUpdateLeft">
+                        <div className="userUpdateItem">
+                            <label>Username</label>
+                            <input 
+                            type="text" 
+                            placeholder="annabombo99" 
+                            className="userUpdateInput" 
+                            />
+                        </div>
+                        <div className="userUpdateItem">
+                            <label>Full Name</label>
+                            <input type="text" 
+                            placeholder="Anna Bombo" 
+                            className="userUpdateInput" 
+                            />
+                        </div>
+                        <div className="userUpdateItem">
+                            <label>Email</label>
+                            <input 
+                            type="text" 
+                            placeholder="annabombo@gmail.com" 
+                            className="userUpdateInput" 
+                            />
+                        </div>
+                        <div className="userUpdateItem">
+                            <label>Phone</label>
+                            <input 
+                            type="text" 
+                            placeholder="+49 1234 567 89" 
+                            className="userUpdateInput" 
+                            />
+                        </div>
+                        <div className="userUpdateItem">
+                            <label>Address</label>
+                            <input 
+                            type="text" 
+                            placeholder="Sachsen Anhalt | Germany" 
+                            className="userUpdateInput" 
+                            />
+                        </div>
+                    </div>
+                    <div className="userUpdateRight">
+                        <div className="userUpdateUpload">
+                           <img className="userUpdateImg" 
+                           src="https://images.pexels.com/photos/46317/pexels-photo-46317.jpeg" 
+                           alt="" 
+                           />
+                           <label htmlFor="file">
+                               <Publish className="userUpdateIcon"/>
+                           </label>
+                           <input type="file" id="file" style={{ display:"none" }} /> 
+                        </div>
+                        <button className="userUpdateButton">Update</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
   );
